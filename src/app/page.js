481a9ce1fff81e4ca5a298a48eede66483f5706c
Olivier Larose <olivierlarose1@gmail.com>
2023-05-17 12:28:12 -0400

    first ocmmit

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,44 @@
-import Image from 'next/image'
+'use client';
 import styles from './page.module.css'
+import { useEffect, useRef } from 'react';
 
 export default function Home() {
+
+  const sticky = useRef(null);
+  const container = useRef(null);
+  const initialMaskSize = .8;
+  const targetMaskSize = 30;
+  const easing = 0.15;
+  let easedScrollProgress = 0;
+
+  useEffect( () => {
+    requestAnimationFrame(animate)
+  }, [])
+
+  const animate = () => {
+    const maskSizeProgress = targetMaskSize * getScrollProgress();
+    sticky.current.style.webkitMaskSize = (initialMaskSize + maskSizeProgress) * 100 + "%";
+    requestAnimationFrame(animate)
+  }
+
+  const getScrollProgress = () => {
+    const scrollProgress = sticky.current.offsetTop / (container.current.getBoundingClientRect().height - window.innerHeight)
+    const delta = scrollProgress - easedScrollProgress;
+    easedScrollProgress += delta * easing;
+    return easedScrollProgress
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div ref={container} className={styles.container}>
+        <div ref={sticky} className={styles.stickyContainer}>
+          <video autoPlay muted loop>
+            <source src="/medias/nature.mp4" type="video/mp4"/>
+          </video>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.footer}>
+        <h2></h2>
       </div>
     </main>
   )
